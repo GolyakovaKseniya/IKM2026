c14456a25ababd99f3b729139784108a5aafbcb9
ReshetnikovaGolyakovaIT1516 <golakovak3@gmail.com>
2026-01-12 20:04:32 +0500

    Изменение

--- src/main/java/org/example/library/Main.java
+++ src/main/java/org/example/library/Main.java
@@ -1,7 +1,15 @@
 package org.example.library;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+ * Главный класс Spring Boot приложения.
+ * Точка входа для запуска всего приложения.
+ *
+ * <p>Этот класс инициализирует Spring Application Context,
+ * запускает встроенный веб-сервер и настраивает автоматическую конфигурацию.</p>
+ * SpringBootApplication - помечает класс как конфигурационный, настраивает приложение
+ * на основе зависимостей, настраивает базу данных, шаблонизатор, регистрирует все компоненты
+ */
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
